d6e16dc18ae9fe64979604663888c582e99df8e3
MeAlam1 <schon@LAPTOP-8D7UO8FO>
2023-07-01 17:55:00 +0200

    Taming Process(almost done)

---
+++ src/main/java/net/mcreator/howtoownadragon/procedures/KickWhenNotTamedProcedure.java
@@ -0,0 +1,27 @@
+package net.mcreator.howtoownadragon.procedures;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+import net.mcreator.howtoownadragon.HowToOwnADragonMod;
+
+public class KickWhenNotTamedProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity.isVehicle() && !(entity instanceof TamableAnimal _tamEnt ? _tamEnt.isTame() : false)) {
+			HowToOwnADragonMod.queueServerWork(5, () -> {
+				if (world instanceof ServerLevel _level)
+					_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+							"tp @p ~ ~ ~2");
+			});
+		}
+	}
+}
