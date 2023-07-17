f7fd265d07e7eeb5f9cf5fc3494cde966bf00f5f
Justindraak1 <justi@Laptop_Justin.home>
2023-07-17 16:30:14 +0200

    nope

--- src/main/java/net/mcreator/howtoownadragon/block/ReaperSpawnBlockBlock.java
+++ src/main/java/net/mcreator/howtoownadragon/block/ReaperSpawnBlockBlock.java
@@ -6,22 +6,26 @@ import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.block.IronBarsBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.util.RandomSource;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.core.BlockPos;
+
+import net.mcreator.howtoownadragon.procedures.ReaperSpawnBlockUpdateTickProcedure;
 
 import java.util.List;
 import java.util.Collections;
 
-public class RustyDragonProofMetalBarsBlock extends IronBarsBlock {
-	public RustyDragonProofMetalBarsBlock() {
-		super(BlockBehaviour.Properties.of(Material.STONE).sound(SoundType.GRAVEL).strength(-1, 3600000));
+public class ReaperSpawnBlockBlock extends Block {
+	public ReaperSpawnBlockBlock() {
+		super(BlockBehaviour.Properties.of(Material.WOOD).sound(SoundType.WOOD).strength(1f, 10f));
 	}
 
 	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 0;
+		return 15;
 	}
 
 	@Override
@@ -30,5 +34,14 @@ public class RustyDragonProofMetalBarsBlock extends IronBarsBlock {
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(this, 1));
+	}
+
+	@Override
+	public void tick(BlockState blockstate, ServerLevel world, BlockPos pos, RandomSource random) {
+		super.tick(blockstate, world, pos, random);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		ReaperSpawnBlockUpdateTickProcedure.execute(world, x, y, z);
 	}
 }
---
+++ src/main/java/net/mcreator/howtoownadragon/world/features/ReaperSpawningStructureFeature.java
@@ -0,0 +1,60 @@
+
+package net.mcreator.howtoownadragon.world.features;
+
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockIgnoreProcessor;
+import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.BlockPos;
+
+import net.mcreator.howtoownadragon.procedures.ReaperSpawningStructureOnStructureInstanceGeneratedProcedure;
+
+import java.util.Set;
+
+public class ReaperSpawningStructureFeature extends Feature<NoneFeatureConfiguration> {
+	private final Set<ResourceKey<Level>> generate_dimensions = Set.of(Level.OVERWORLD);
+	private StructureTemplate template = null;
+
+	public ReaperSpawningStructureFeature() {
+		super(NoneFeatureConfiguration.CODEC);
+	}
+
+	@Override
+	public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context) {
+		if (!generate_dimensions.contains(context.level().getLevel().dimension()))
+			return false;
+		if (template == null)
+			template = context.level().getLevel().getStructureManager().getOrCreate(new ResourceLocation("how_to_own_a_dragon", "empty_reaper"));
+		if (template == null)
+			return false;
+		boolean anyPlaced = false;
+		if ((context.random().nextInt(1000000) + 1) <= 1000) {
+			int count = context.random().nextInt(1) + 1;
+			for (int a = 0; a < count; a++) {
+				int i = context.origin().getX() + context.random().nextInt(16);
+				int k = context.origin().getZ() + context.random().nextInt(16);
+				int j = context.level().getHeight(Heightmap.Types.WORLD_SURFACE_WG, i, k) - 1;
+				BlockPos spawnTo = new BlockPos(i + 0, j + 0, k + 0);
+				WorldGenLevel world = context.level();
+				int x = spawnTo.getX();
+				int y = spawnTo.getY();
+				int z = spawnTo.getZ();
+				if (template.placeInWorld(context.level(), spawnTo, spawnTo, new StructurePlaceSettings().setMirror(Mirror.values()[context.random().nextInt(2)]).setRotation(Rotation.values()[context.random().nextInt(3)]).setRandom(context.random())
+						.addProcessor(BlockIgnoreProcessor.STRUCTURE_BLOCK).setIgnoreEntities(false), context.random(), 2)) {
+					ReaperSpawningStructureOnStructureInstanceGeneratedProcedure.execute(world, x, y, z);
+					anyPlaced = true;
+				}
+			}
+		}
+		return anyPlaced;
+	}
+}
