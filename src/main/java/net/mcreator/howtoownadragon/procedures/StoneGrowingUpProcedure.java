b8abac1a51720a94a1a29fafd8d5134fc2a1be23
MeAlam1 <schon@LAPTOP-8D7UO8FO>
2023-07-21 14:30:03 +0200

    Sync

--- src/main/java/net/mcreator/howtoownadragon/procedures/StoneGrowingUpProcedure.java
+++ src/main/java/net/mcreator/howtoownadragon/procedures/StoneGrowingUpProcedure.java
@@ -5,7 +5,20 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.core.particles.ParticleTypes;
+
+import net.mcreator.howtoownadragon.init.HowToOwnADragonModEntities;
+import net.mcreator.howtoownadragon.entity.JuvenileMeatlugEntity;
+import net.mcreator.howtoownadragon.entity.BabyMeatlugEntity;
 
 import javax.annotation.Nullable;
 
@@ -15,17 +28,37 @@ public class StoneGrowingUpProcedure {
 	public static void onRightClickEntity(PlayerInteractEvent.EntityInteract event) {
 		if (event.getHand() != event.getEntity().getUsedItemHand())
 			return;
-		execute(event, event.getTarget(), event.getEntity());
+		execute(event, event.getLevel(), event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getTarget(), event.getEntity());
 	}
 
-	public static void execute(Entity entity, Entity sourceentity) {
-		execute(null, entity, sourceentity);
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
+		execute(null, world, x, y, z, entity, sourceentity);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity, Entity sourceentity) {
+	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
-		if (true) {
+		if (entity instanceof BabyMeatlugEntity) {
+			if ((sourceentity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Blocks.STONE.asItem()) {
+				if (sourceentity instanceof Player _player) {
+					ItemStack _stktoremove = new ItemStack(Blocks.STONE);
+					_player.getInventory().clearOrCountMatchingItems(p -> _stktoremove.getItem() == p.getItem(), 1, _player.inventoryMenu.getCraftSlots());
+				}
+				if (world instanceof ServerLevel _level)
+					_level.sendParticles(ParticleTypes.HAPPY_VILLAGER, x, y, z, 30, 1, 1, 1, 0.5);
+				if (!entity.level.isClientSide())
+					entity.discard();
+				if (world instanceof ServerLevel _level) {
+					Entity entityToSpawn = new JuvenileMeatlugEntity(HowToOwnADragonModEntities.JUVENILE_MEATLUG.get(), _level);
+					entityToSpawn.moveTo(x, y, z, 0, 0);
+					entityToSpawn.setYBodyRot(0);
+					entityToSpawn.setYHeadRot(0);
+					entityToSpawn.setDeltaMovement(0, 0, 0);
+					if (entityToSpawn instanceof Mob _mobToSpawn)
+						_mobToSpawn.finalizeSpawn(_level, _level.getCurrentDifficultyAt(entityToSpawn.blockPosition()), MobSpawnType.MOB_SUMMONED, null, null);
+					_level.addFreshEntity(entityToSpawn);
+				}
+			}
 		}
 	}
 }
