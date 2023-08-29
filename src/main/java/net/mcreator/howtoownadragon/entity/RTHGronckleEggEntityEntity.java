42886ae18c09094710ab2a9626022b8d11f07606
MeAlam1 <schon@Aram>
2023-08-29 18:59:49 +0200

    fixed gronckle egg immunity

--- src/main/java/net/mcreator/howtoownadragon/entity/RTHGronckleEggEntityEntity.java
+++ src/main/java/net/mcreator/howtoownadragon/entity/RTHGronckleEggEntityEntity.java
@@ -6,6 +6,8 @@ import net.minecraftforge.network.NetworkHooks;
 
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -14,6 +16,9 @@ import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.AreaEffectCloud;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
@@ -48,6 +53,33 @@ public class RTHGronckleEggEntityEntity extends PathfinderMob {
 	@Override
 	public boolean removeWhenFarAway(double distanceToClosestPlayer) {
 		return false;
+	}
+
+	@Override
+	public boolean hurt(DamageSource source, float amount) {
+		if (source.is(DamageTypes.IN_FIRE))
+			return false;
+		if (source.getDirectEntity() instanceof AbstractArrow)
+			return false;
+		if (source.getDirectEntity() instanceof Player)
+			return false;
+		if (source.getDirectEntity() instanceof ThrownPotion || source.getDirectEntity() instanceof AreaEffectCloud)
+			return false;
+		if (source.is(DamageTypes.CACTUS))
+			return false;
+		if (source.is(DamageTypes.DROWN))
+			return false;
+		if (source.is(DamageTypes.LIGHTNING_BOLT))
+			return false;
+		if (source.is(DamageTypes.TRIDENT))
+			return false;
+		if (source.is(DamageTypes.DRAGON_BREATH))
+			return false;
+		if (source.is(DamageTypes.WITHER))
+			return false;
+		if (source.is(DamageTypes.WITHER_SKULL))
+			return false;
+		return super.hurt(source, amount);
 	}
 
 	@Override
