5794c30ccb3ebd49c333626ede28a812396853f9
MeAlam1 <schon@Aram>
2023-08-30 22:21:39 +0200

    fixed valka staff bug

--- src/main/java/net/mcreator/howtoownadragon/client/gui/MainPageInfoGronckleScreen.java
+++ src/main/java/net/mcreator/howtoownadragon/client/gui/MainPageInfoGronckleScreen.java
@@ -1,13 +1,25 @@
 package net.mcreator.howtoownadragon.client.gui;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.gui.components.ImageButton;
+
+import net.mcreator.howtoownadragon.world.inventory.MainPageInfoGronckleMenu;
+
+import java.util.HashMap;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+
 public class MainPageInfoGronckleScreen extends AbstractContainerScreen<MainPageInfoGronckleMenu> {
-
 	private final static HashMap<String, Object> guistate = MainPageInfoGronckleMenu.guistate;
-
 	private final Level world;
 	private final int x, y, z;
 	private final Player entity;
-
 	ImageButton imagebutton_food_button_infolens_90x30;
 
 	public MainPageInfoGronckleScreen(MainPageInfoGronckleMenu container, Inventory inventory, Component text) {
@@ -33,7 +45,6 @@ public class MainPageInfoGronckleScreen extends AbstractContainerScreen<MainPage
 		this.renderBackground(ms);
 		super.render(ms, mouseX, mouseY, partialTicks);
 		this.renderTooltip(ms, mouseX, mouseY);
-
 	}
 
 	@Override
@@ -41,7 +52,6 @@ public class MainPageInfoGronckleScreen extends AbstractContainerScreen<MainPage
 		RenderSystem.setShaderColor(1, 1, 1, 1);
 		RenderSystem.enableBlend();
 		RenderSystem.defaultBlendFunc();
-
 		RenderSystem.setShaderTexture(0, texture);
 		this.blit(ms, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
 
@@ -57,7 +67,6 @@ public class MainPageInfoGronckleScreen extends AbstractContainerScreen<MainPage
 			this.minecraft.player.closeContainer();
 			return true;
 		}
-
 		return super.keyPressed(key, b, c);
 	}
 
@@ -78,13 +87,9 @@ public class MainPageInfoGronckleScreen extends AbstractContainerScreen<MainPage
 	@Override
 	public void init() {
 		super.init();
-
 		imagebutton_food_button_infolens_90x30 = new ImageButton(this.leftPos + 12, this.topPos + 12, 90, 30, 0, 0, 30, new ResourceLocation("how_to_own_a_dragon:textures/screens/atlas/imagebutton_food_button_infolens_90x30.png"), 90, 60, e -> {
 		});
-
 		guistate.put("button:imagebutton_food_button_infolens_90x30", imagebutton_food_button_infolens_90x30);
 		this.addRenderableWidget(imagebutton_food_button_infolens_90x30);
-
 	}
-
 }
