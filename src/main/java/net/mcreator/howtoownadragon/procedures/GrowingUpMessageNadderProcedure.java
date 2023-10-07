bae264519e23d0d4ab40c6c8439eea3d8c3fe43f
MeAlam1 <aram.schonhage@hva.nl>
2023-10-07 12:02:18 +0200

    Part 9, Deadly Nadder, Growing Up Stages :D

---
+++ src/main/java/net/mcreator/howtoownadragon/procedures/GrowingUpMessageNadderProcedure.java
@@ -0,0 +1,34 @@
+package net.mcreator.howtoownadragon.procedures;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.Mth;
+import net.minecraft.network.chat.Component;
+
+public class GrowingUpMessageNadderProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if (Mth.nextInt(RandomSource.create(), 1, 4) == 1) {
+			if ((entity instanceof TamableAnimal _tamEnt ? (Entity) _tamEnt.getOwner() : null) instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal(("Your " + entity.getDisplayName().getString() + " is ready to grow up! It requires some sustenance. It needs some Raw Mutton!")), false);
+			entity.getPersistentData().putString("naddergrowup", "mutton");
+		} else if (Mth.nextInt(RandomSource.create(), 1, 4) == 2) {
+			if ((entity instanceof TamableAnimal _tamEnt ? (Entity) _tamEnt.getOwner() : null) instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal(("Your " + entity.getDisplayName().getString() + " is ready to grow up! It requires some sustenance. It needs some Raw Cod!")), false);
+			entity.getPersistentData().putString("naddergrowup", "cod");
+		} else if (Mth.nextInt(RandomSource.create(), 1, 4) == 3) {
+			if ((entity instanceof TamableAnimal _tamEnt ? (Entity) _tamEnt.getOwner() : null) instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal(("Your " + entity.getDisplayName().getString() + " is ready to grow up! It requires some sustenance. It needs some Raw Salmon!")), false);
+			entity.getPersistentData().putString("naddergrowup", "salmon");
+		} else if (Mth.nextInt(RandomSource.create(), 1, 4) == 4) {
+			if ((entity instanceof TamableAnimal _tamEnt ? (Entity) _tamEnt.getOwner() : null) instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal(("Your " + entity.getDisplayName().getString() + " is ready to grow up! It requires some sustenance. It needs some Cooked Chicken!")), false);
+			entity.getPersistentData().putString("naddergrowup", "chicken");
+		} else {
+			GrowingUpMessageNadderProcedure.execute(entity);
+		}
+	}
+}
