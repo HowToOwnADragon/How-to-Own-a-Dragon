57d03d656ee8f453764fc7591801fb7e9a4a5f94
MeAlam1 <aram.schonhage@hva.nl>
2023-12-07 13:24:14 +0100

    Fixed #47

--- src/main/java/net/mcreator/howtoownadragon/procedures/GiveEmptyBucketYaknogProcedure.java
+++ src/main/java/net/mcreator/howtoownadragon/procedures/GiveEmptyBucketYaknogProcedure.java
@@ -11,26 +11,30 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Entity;
 
+import net.mcreator.howtoownadragon.init.HowToOwnADragonModItems;
+
 import javax.annotation.Nullable;
 
 @Mod.EventBusSubscriber
 public class GiveEmptyBucketYaknogProcedure {
 	@SubscribeEvent
 	public static void onItemCrafted(PlayerEvent.ItemCraftedEvent event) {
-		execute(event, event.getEntity());
+		execute(event, event.getEntity(), event.getCrafting());
 	}
 
-	public static void execute(Entity entity) {
-		execute(null, entity);
+	public static void execute(Entity entity, ItemStack itemstack) {
+		execute(null, entity, itemstack);
 	}
 
-	private static void execute(@Nullable Event event, Entity entity) {
+	private static void execute(@Nullable Event event, Entity entity, ItemStack itemstack) {
 		if (entity == null)
 			return;
-		if (entity instanceof Player _player) {
-			ItemStack _setstack = new ItemStack(Items.BUCKET);
-			_setstack.setCount(3);
-			ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+		if (itemstack.getItem() == HowToOwnADragonModItems.YAK_NOG_BOWL.get()) {
+			if (entity instanceof Player _player) {
+				ItemStack _setstack = new ItemStack(Items.BUCKET);
+				_setstack.setCount(3);
+				ItemHandlerHelper.giveItemToPlayer(_player, _setstack);
+			}
 		}
 	}
 }
